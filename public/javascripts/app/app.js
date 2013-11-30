56f7ab2df2c81dd5274c85a08d53bf53a86b8b64
Lars Soderkvist <updates11@mac.com>
2013-11-30 17:26:19 -0600

    Refactored to streamline card drawing and kill extraneous divs.

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -66,14 +66,12 @@ function htmlInitiateGame(game){
     //*   to a series of divs, each with class of 'available' and a class with the code that
     //*   defines its shapes and colors for styling purposes (though it can also be used for
     //*   targeting the element rather than weighing each div down with separate data for that)
-  var pairs = _.map(game.hand, function(h, i){return '<div class="footprint hidden"><div class="' + game.hand[i] + ' available"></div></div>';});
+  var pairs = _.map(game.hand, function(h, i){return '<div class="' + game.hand[i] + ' available"></div>';});
+  // var pairs = _.map(game.hand, function(h, i){return '<div class="footprint hidden"><div class="' + game.hand[i] + ' available"></div></div>';});
     //* add the card to the hand area
   $('#hand').append(pairs);
     //* give the parent div a data attribute of the game id
   $('#hand').attr('data-id', game.id);
-    //* reveal the footprint
-    //*   this step taken to avoid the box-shadow appearing before the card does on page load
-  $('#footprint').removeClass('hidden');
 }
 
   // * called by checkForMatch if a match is found
